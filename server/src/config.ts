f615801e3e62724037cf52aacc382a2711fb3734
Denis Onder <denis_onder@protonmail.com>
2022-06-03 10:06:38 +0200

    feat: jwt util functions

---
+++ server/src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  port: process.env.PORT || 9000,
+  mongoDbUri: process.env.MONGO_DB_URI || "mongodb://localhost:27017/test",
+  secret: process.env.SECRET || "secret",
+};
+
+export default config;
